2dfdcff560414280bf36a10f484f34fd4f797715
AntoineWisd <122507207+AntoineWisd@users.noreply.github.com>
2023-01-31 10:17:49 +0700

    Create lego-Tx-count.py

---
+++ lego-Tx-count.py
@@ -0,0 +1,21 @@
+# Preliminary work
+
+class WalletTxCountLego:
+    def __init__(self, min_tx_count=3):
+        self.min_tx_count = min_tx_count
+
+    def check_address(self, address):
+        # Retrieve the number of transactions for the address using a blockchain API
+        tx_count = get_tx_count(address)
+        # Check if the number of transactions for the address is greater than the minimum limit configured
+        if tx_count >= self.min_tx_count:
+            return True
+        else:
+            return False
+
+# This code creates a WalletTxCountLego class which takes in a configuration parameter for the minimum number of transactions for an address to be 
+# considered valid (default is 3 transactions). The check_address method takes in an Ethereum address and uses a fictional function get_tx_count to 
+# retrieve the number of transactions for that address using a blockchain API. It then checks if the number of transactions for the address is greater
+# than the minimum limit configured and returns True if it is, False otherwise.
+
+# get_tx_count function would need to be replaced with a function that uses a real API to retrieve the number of transactions for an address.
